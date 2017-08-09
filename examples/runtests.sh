fa6cf6059f9e5abe5006bc20ef3569ebf2373953
Pedro Fonseca <pedro@hecticant.com>
2017-08-10 00:04:52 +0100

    Update xcodebuild invocation to use the latest simulator.

--- examples/runtests.sh
+++ examples/runtests.sh
@@ -4,7 +4,7 @@ pushd $(dirname -- "$0")
 
 RESULT_PATH=`mktemp -d` && rm -r $RESULT_PATH
 TEST_TARGET='xcuistringer-sample'
-xcodebuild test -project $TEST_TARGET.xcodeproj -scheme $TEST_TARGET -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.1' -resultBundlePath $RESULT_PATH
+xcodebuild test -project $TEST_TARGET.xcodeproj -scheme $TEST_TARGET -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' -resultBundlePath $RESULT_PATH
 ../transform.py $RESULT_PATH/1_Test
 
 popd
